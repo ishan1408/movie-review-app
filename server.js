263d6bd90052e434c23f2e8a0d6d75ae47d34118
ishan1408 <ishan@startupflora.com>
2025-04-15 17:31:10 +0530

    second commit

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ app.use(express.json());
 
 app.use("/api/reviews", reviewRoutes);
 
-mongoose
-  .connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
